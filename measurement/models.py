28ade5616457d7d8a5802482026834770590a974
Nikolay Malyutin <xenos-nick@mail.ru>
2022-02-25 20:01:08 +0300

    Созданы модели датчика (Sensor) и измерения (Measurement)

--- measurement/models.py
+++ measurement/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название')
+    description = models.CharField(max_length=255, null=True, blank=True, verbose_name='Описание')
+
+    class Meta:
+        verbose_name = 'Датчик'
+        verbose_name_plural = 'Датчики'
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements', verbose_name='Датчик')
+    temperature = models.DecimalField(max_digits=3, decimal_places=1, verbose_name='Температура')
+    created_at = models.DateTimeField(auto_now=True, verbose_name='Дата измерения')
+
+    class Meta:
+        verbose_name = 'Измерение'
+        verbose_name_plural = 'Измерения'
