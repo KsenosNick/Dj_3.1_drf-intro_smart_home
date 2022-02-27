db6e4fc2bc99c94f7e6024a3f20b63df37781b35
Nikolay Malyutin <xenos-nick@mail.ru>
2022-02-27 19:08:29 +0300

    Созданы обработчики SensorsViewCreate, SensorViewUpdate и MeasurementCreate

--- measurement/views.py
+++ measurement/views.py
@@ -1,2 +1,19 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework import generics
+
+from measurement.models import Sensor, Measurement
+from measurement.serializers import SensorSerializer, SensorDetailSerializer, MeasurementSerializer
+
+
+class SensorsViewCreate(generics.ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class SensorViewUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+
+class MeasurementCreate(generics.CreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
