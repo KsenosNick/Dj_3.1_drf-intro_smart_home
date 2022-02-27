616d8020b1f4645c777432d948cb33d081828161
Nikolay Malyutin <xenos-nick@mail.ru>
2022-02-27 19:09:02 +0300

    Зарегистрированы маршруты

--- measurement/urls.py
+++ measurement/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from measurement.views import SensorsViewCreate, SensorViewUpdate, MeasurementCreate
+
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorsViewCreate.as_view()),
+    path('sensors/<pk>/', SensorViewUpdate.as_view()),
+    path('measurements/', MeasurementCreate.as_view()),
 ]
